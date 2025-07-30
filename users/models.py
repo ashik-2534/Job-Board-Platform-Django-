3e28b9766e913bdd1eebbed828d6bd1ee651dee6
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-07-30 10:57:08 +0600

    added get absolute url

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Profile(models.Model):
@@ -13,3 +14,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+    
+    def get_absolute_url(self):
+        """Return the URL to this profile's public page"""
+        return reverse('profile-show-user', kwargs={'username': self.user.username})
